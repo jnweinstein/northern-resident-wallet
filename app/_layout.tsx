7d8a267406ce7f71e6638b058c0f0085c956f2b5
Eric Peng <eric.peng.1@stonybrook.edu>
2024-03-18 17:08:58 -0400

    updated dependencies, fixed configs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import '../tamagui-web.css'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 
 import { Stack } from 'expo-router'
@@ -6,13 +5,13 @@ import { Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
 
 import { TamaguiProvider } from 'tamagui'
-import { tamaguiConfig } from '../tamagui.config'
+import tamaguiConfig from '../tamagui.config'
 export default function RootLayout() {
 
     const colorScheme = useColorScheme()
     return (
         // add this
-        <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme}>
+        <TamaguiProvider config={tamaguiConfig}>
             <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
                 <Stack>
                     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
