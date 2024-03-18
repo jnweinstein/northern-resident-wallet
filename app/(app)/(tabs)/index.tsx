60d4c07c44f0b95aac93d20d0899b4f140f873f5
Jacob Weinstein <gargoyle6283@gmail.com>
2024-03-18 15:20:41 -0400

    update home page

---
+++ app/(app)/(tabs)/index.tsx
@@ -0,0 +1,45 @@
+// home screen
+import type { CardProps } from 'tamagui'
+
+import { Button, Card, H2, Image, Paragraph, YStack, XStack } from 'tamagui'
+
+type BalanceProps = {
+    name: string,
+    balance: number
+}
+
+export function CardDemo() {
+  return (
+    <YStack $sm={{ flexDirection: 'column' }} paddingHorizontal="$4" space>
+      <DemoCard
+        animation="bouncy"
+        size="$4"
+        width={250}
+        height={300}
+        scale={0.9}
+        hoverStyle={{ scale: 0.925 }}
+        pressStyle={{ scale: 0.875 }}
+      />
+      <DemoCard size="$5" width={250} height={300} />
+    </YStack>
+  )
+}
+export function DemoCard(props: CardProps, name: string) {
+  return (
+    <Card elevate size="$4" bordered {...props}>
+      <Card.Header padded>
+        <H2>{name}</H2>
+        <Paragraph theme="alt2">Balance</Paragraph>
+      </Card.Header>
+      
+      <Card.Footer padded>
+        <XStack flex={1} />
+        <Button borderRadius="$10">Purchase</Button>
+      </Card.Footer>
+      <Card.Background>
+      </Card.Background>
+    </Card>
+
+  )
+
+}
