ace941d78049f8e6ef859d0534c9405a2a16b78f
Katherine Wu <katherinewu312@yahoo.com>
2024-03-20 13:49:01 -0400

    stack navigator for send/receive

--- app/(app)/(tabs)/transfer.tsx
+++ app/(app)/(tabs)/transfer.tsx
@@ -1,42 +1,55 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { TamaguiProvider, Button } from 'tamagui';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'tamagui'
 
 export default function Tab() {
-  const navigation = useNavigation();
 
-  // Component for the Send Page
-  const SendPage = () => {
+  function SendScreen() {
     return (
-      <div>
-        <h1>Send Bitcoin</h1>
-        {/* Additional content for the Send Page */}
-      </div>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Send screen</Text>
+      </View>
     );
-  };
+  }
+  
+  function ReceiveScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Receive screen</Text>
+      </View>
+    );
+  }
+  
+  function TransferHomeScreen({ navigation }: {navigation: any}) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button alignSelf="center" size="$5" onPress={() => navigation.navigate('Send')}>
+          <Text style={{ fontWeight: 'bold', fontSize: 24 }}>Send</Text>
+          <Text>To an external BTC address</Text>
+        </Button>
+        <Button alignSelf="center" size="$5" onPress={() => navigation.navigate('Receive')}>
+          <Text style={{ fontWeight: 'bold', fontSize: 24 }}>Receive</Text>
+          <Text>By sharing your BTC address</Text>
+        </Button>
+      </View>
+    );
+  }
 
-  // Component for the Receive Page
-  const ReceivePage = () => {
-    return (
-      <div>
-        <h1>Receive Bitcoin</h1>
-        {/* Additional content for the Receive Page */}
-      </div>
-    );
-  };
+  const TransferStack = createNativeStackNavigator();
+
+
+  return (
+    <TransferStack.Navigator>
+      <TransferStack.Screen name=" " component={TransferHomeScreen} />
+      <TransferStack.Screen name="Send" component={SendScreen} />
+      <TransferStack.Screen name="Receive" component={ReceiveScreen} />
+    </TransferStack.Navigator>
+  );
+
+
 
   /*
-  const handleSendClick = () => {
-    navigation.navigate('SendPage');
-  };
-
-  // Function to handle navigation to Receive Page
-  const handleReceiveClick = () => {
-    navigation.navigate('ReceivePage');
-  };
-  */
-
   return (
     <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         <Button alignSelf="center" size="$5">
@@ -49,4 +62,5 @@ export default function Tab() {
       </Button>
     </View>
   );
+  */
 }
