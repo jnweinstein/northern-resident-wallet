fb1e56994ac99a769ef0e40af3044f4b627a7e3f
Eric Peng <eric.peng.1@stonybrook.edu>
2024-03-20 16:09:54 -0400

    settings avatar

--- app/(app)/(tabs)/settings.tsx
+++ app/(app)/(tabs)/settings.tsx
@@ -1,10 +1,16 @@
-import { View, Text, Button } from 'tamagui';
+import { View, Button, XStack, Avatar,  H6 } from 'tamagui';
 import { useSession } from '../../../ctx';
 
 export default function Tab() {
   const { signOut } = useSession();
   return (
     <View style={{ justifyContent: 'left', alignItems: 'left', flex: 1, margin: '1em' }}>
+      <XStack alignContent='center' alignItems='center' space>
+        <Avatar circular size="$4">
+          <Avatar.Fallback backgroundColor="grey" />
+        </Avatar>
+        <H6>(username)</H6>
+      </XStack>
       <Button
         onPress={() => {
           signOut();
